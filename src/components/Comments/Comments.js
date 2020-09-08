d8ceae1fccbe6b870df330860c4724429cce32b0
Tzong-Lian-Tsay <tzong-lian-tsay@lambdastudents.com>
2020-09-08 18:59:52 -0400

    MVP complete with Add Comments Stretch Complete

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react'
 import Comment from './Comment';
+import {v4 as uuid } from 'uuid'
 import './Comments.css';
+
 
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
+  
+  const [inputComment, setInputComment] = useState('')
+  const changeHandler = evt=>{
+    setInputComment(evt.target.value)
+  }
+  const submitHandler = evt=>{
+    evt.preventDefault()
+    console.log(inputComment)
+    setInputComment(comments.push(
+      {
+        id: uuid(),
+        username: 'dummy_user',
+        text: inputComment
+      }
+    ))
+    console.log(comments)
+  }
+
+
 
   return (
     <div>
@@ -17,7 +38,22 @@ const Comments = props => {
           />
         )
       })
-      }
+      }  
+
+      {/* Stretch:  Add Comments */}
+      <form className='comment-form' onSubmit={evt => 
+        submitHandler(evt)
+        }>
+        <label>
+          <input 
+            className='input'
+            type='text'
+            placeholder='Add a comment...'
+            onChange={evt => changeHandler(evt)}
+          />
+        </label>
+        <button>Submit!</button>
+      </form>
     </div>
   );
 };
